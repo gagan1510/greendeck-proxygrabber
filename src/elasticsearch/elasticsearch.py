5cdeb2347f85ed1841a707a7836a80c08f2d5569
IIITian-Chandan <cmishra646@gmail.com>
2019-08-07 16:43:43 +0530

    initial commit

--- src/elasticsearch/elasticsearch.py
+++ src/elasticsearch/elasticsearch.py
@@ -8,5 +8,8 @@ import sys
 
 class ElasticSearch:
     def __init__(self, ECS_HOST, ECS_INDEX, ECS_TYPE, username=None, password=None):
+        self.ECS_HOST = ECS_HOST
+        self.ECS_INDEX = ECS_INDEX
+        self.ECS_TYPE = ECS_TYPE
         self.password = password
         self.username = username
